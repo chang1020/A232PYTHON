38a4953ba40cba64869f120e76c18dbd7b9e1309
chang <165901402+chang1020@users.noreply.github.com>
2024-05-18 16:28:38 +0800

    Add files via upload

---
+++ tuples&list.py
@@ -0,0 +1,12 @@
+Food=("nasi lemak","ayam goreng","roti canai") 
+print(Food)
+Number =[33,88,97,88,42,75,56]
+Number[-1]=25 #-1 =last
+c=list(Food)#change tuple to list
+del(c[1])
+print(len(Number))
+print(c)
+#() Tuple - unchange
+#Only way to delete tuple is change the type
+#[] List
+#{} Set
